28ef1a0887d02f51d9e82a90df4fe926876443d7
cofd <cofd@localhost>
2010-05-10 11:00:58 +0800

    tiny

---
+++ src/Logging/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Kiss.Logging
+{
+    [Serializable]
+    [OriginalEntityName("gLog")]
+    public class Log : QueryObject<Log, int>
+    {
+        public string SiteKey { get; set; }
+        public string Logger { get; set; }
+        public string Message { get; set; }
+        public string LogLevel { get; set; }
+        public DateTime DateCreate { get; set; }
+    }
+}
