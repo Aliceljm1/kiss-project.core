8b97ddff344128957aae628e2248ab0aa179d7b0
cofd <cofd@localhost>
2010-05-10 11:00:52 +0800

    add query condition plugin

---
+++ src/Query/QueryInitializer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Kiss.Plugin;
+using Kiss.Utils;
+
+namespace Kiss.Query
+{
+    [AutoInit(Title = "query")]
+    public class QueryInitializer : IPluginInitializer
+    {
+        #region IPluginInitializer Members
+
+        public void Init(ServiceLocator sl, PluginSetting setting)
+        {
+            if (!setting.Enable) return;
+
+            string type = setting["type"];
+
+            if (StringUtil.IsNullOrEmpty(type))
+            {
+                setting.Enable = false;
+                return;
+            }
+
+            sl.AddComponent("kiss.query", typeof(IQueryPlugin), Type.GetType(type, true, true));
+        }
+
+        #endregion
+    }
+}
