6bba5962963a096761f1348745fb39ce57f85761
zhli <zhli@localhost>
2010-06-03 09:48:14 +0800

    2.6.2 and 2.6.3

---
+++ readme.txt
@@ -0,0 +1,12 @@
+KISS core
+
+核心的一些接口
+
+版本历史：
+v2.6.2
+支持对模型类设置连接字符串
+从Logging Component迁移了LogInitializer
+ServerUtil的小改动（部分方法增加了HttpResponse参数）
+
+v2.6.3
+迁移到vs2010
---
+++ src/Logging/LogInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Kiss.Plugin;
+
+namespace Kiss.Logging
+{
+    [AutoInit(Title = "Log", Priority = 99)]
+    public class LogInitializer : IPluginInitializer
+    {
+        public void Init(ServiceLocator sl, PluginSetting setting)
+        {
+            if (!setting.Enable) return;
+
+            string type = setting["type"] ?? string.Empty;
+            if (string.IsNullOrEmpty(type))
+                return;
+
+            sl.AddComponent("kiss.logger", typeof(ILoggerFactory), Type.GetType(type, true, true));
+        }
+    }
+}
