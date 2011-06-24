da37ba8b37f2c930b8a54a3f503f720302f9d7be
zhli <zhli@localhost>
2011-06-24 22:15:02 +0800

    http://jira.txtek.com/browse/KISSPROJECT-73

--- src/Utils/FileUtil.cs
+++ src/Utils/FileUtil.cs
@@ -3,16 +3,17 @@ using System.Collections;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Kiss.Utils
 {
     public sealed class FileUtil
     {
         // The "_mkdir" function is used by the "CreateDirectory" method.
-        [DllImport ( "msvcrt.dll", SetLastError = true )]
-        private static extern int _mkdir ( string path );
+        [DllImport("msvcrt.dll", SetLastError = true)]
+        private static extern int _mkdir(string path);
 
-        private FileUtil ( )
+        private FileUtil()
         { }
 
         /// <summary>
@@ -31,29 +32,29 @@ namespace Kiss.Utils
         ///		replacement to solve this problem.
         ///		</para>
         /// </remarks>
-        public static DirectoryInfo CreateDirectory ( string path )
+        public static DirectoryInfo CreateDirectory(string path)
         {
             // Create the directory info object for that dir (normalized to its absolute representation).
-            DirectoryInfo oDir = new DirectoryInfo ( Path.GetFullPath ( path ) );
+            DirectoryInfo oDir = new DirectoryInfo(Path.GetFullPath(path));
 
             try
             {
                 // Try to create the directory by using standard .Net features. (#415)
-                if ( !oDir.Exists )
-                    oDir.Create ( );
+                if (!oDir.Exists)
+                    oDir.Create();
 
                 return oDir;
             }
-            catch ( Exception ex )
+            catch (Exception ex)
             {
-                TxtLogger.DumpException ( ex );
-                CreateDirectoryUsingDll ( oDir );
+                TxtLogger.DumpException(ex);
+                CreateDirectoryUsingDll(oDir);
 
-                return new DirectoryInfo ( path );
+                return new DirectoryInfo(path);
             }
         }
 
-        private static void CreateDirectoryUsingDll ( DirectoryInfo dir )
+        private static void CreateDirectoryUsingDll(DirectoryInfo dir)
         {
             // On some occasion, the DirectoryInfo.Create() function will 
             // throw an error due to a bug in the .Net Framework design. For
@@ -62,57 +63,98 @@ namespace Kiss.Utils
             // Create() call will simply fail.
             // To workaround it, we use mkdir directly.
 
-            ArrayList oDirsToCreate = new ArrayList ( );
+            ArrayList oDirsToCreate = new ArrayList();
 
             // Check the entire path structure to find directories that must be created.
-            while ( dir != null && !dir.Exists )
+            while (dir != null && !dir.Exists)
             {
-                oDirsToCreate.Add ( dir.FullName );
+                oDirsToCreate.Add(dir.FullName);
                 dir = dir.Parent;
             }
 
             // "dir == null" means that the check arrives in the root and it doesn't exist too.
-            if ( dir == null )
-                throw ( new System.IO.DirectoryNotFoundException ( "Directory \"" + oDirsToCreate[ oDirsToCreate.Count - 1 ] + "\" not found." ) );
+            if (dir == null)
+                throw (new System.IO.DirectoryNotFoundException("Directory \"" + oDirsToCreate[oDirsToCreate.Count - 1] + "\" not found."));
 
             // Create all directories that must be created (from bottom to top).
-            for ( int i = oDirsToCreate.Count - 1 ; i >= 0 ; i-- )
+            for (int i = oDirsToCreate.Count - 1; i >= 0; i--)
             {
-                string sPath = ( string ) oDirsToCreate[ i ];
-                int iReturn = _mkdir ( sPath );
+                string sPath = (string)oDirsToCreate[i];
+                int iReturn = _mkdir(sPath);
 
-                if ( iReturn != 0 )
-                    throw new ApplicationException ( "Error calling [msvcrt.dll]:_wmkdir(" + sPath + "), error code: " + iReturn );
+                if (iReturn != 0)
+                    throw new ApplicationException("Error calling [msvcrt.dll]:_wmkdir(" + sPath + "), error code: " + iReturn);
             }
         }
 
-        public static bool ArrayContains ( Array array, object value, System.Collections.IComparer comparer )
+        public static bool ArrayContains(Array array, object value, System.Collections.IComparer comparer)
         {
-            foreach ( object item in array )
+            foreach (object item in array)
             {
-                if ( comparer.Compare ( item, value ) == 0 )
+                if (comparer.Compare(item, value) == 0)
                     return true;
             }
             return false;
         }
 
-        public static void CopyDirectory ( string src, string dst, List<string> extensions )
+        public static void CopyDirectory(string src, string dst, List<string> extensions)
         {
-            if ( dst[ dst.Length - 1 ] != Path.DirectorySeparatorChar )
+            if (dst[dst.Length - 1] != Path.DirectorySeparatorChar)
                 dst += Path.DirectorySeparatorChar;
 
-            if ( !Directory.Exists ( dst ) )
-                Directory.CreateDirectory ( dst );
+            if (!Directory.Exists(dst))
+                Directory.CreateDirectory(dst);
 
-            foreach ( string Element in Directory.GetFileSystemEntries ( src ) )
+            foreach (string Element in Directory.GetFileSystemEntries(src))
             {
-                if ( extensions != null && !extensions.Contains ( Path.GetExtension ( Element ) ) )
+                if (extensions != null && !extensions.Contains(Path.GetExtension(Element)))
                     continue;
 
-                if ( Directory.Exists ( Element ) )
-                    CopyDirectory ( Element, dst + Path.GetFileName ( Element ), extensions );
+                if (Directory.Exists(Element))
+                    CopyDirectory(Element, dst + Path.GetFileName(Element), extensions);
                 else
-                    File.Copy ( Element, dst + Path.GetFileName ( Element ), true );
+                    File.Copy(Element, dst + Path.GetFileName(Element), true);
+            }
+        }
+
+        /// <summary>
+        /// 获取文件编码
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static Encoding GetFileEncoding(string filename)
+        {
+            using (FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read))
+            {
+                using (BinaryReader r = new BinaryReader(fs, Encoding.Default))
+                {
+                    byte[] ss = r.ReadBytes(3);
+
+                    //编码类型 Coding=编码类型.ASCII;  
+                    if (ss[0] >= 0xEF)
+                    {
+                        if (ss[0] == 0xEF && ss[1] == 0xBB && ss[2] == 0xBF)
+                        {
+                            return Encoding.UTF8;
+                        }
+                        else if (ss[0] == 0xFE && ss[1] == 0xFF)
+                        {
+                            return Encoding.BigEndianUnicode;
+                        }
+                        else if (ss[0] == 0xFF && ss[1] == 0xFE)
+                        {
+                            return Encoding.Unicode;
+                        }
+                        else
+                        {
+                            return Encoding.Default;
+                        }
+                    }
+                    else
+                    {
+                        return Encoding.Default;
+                    }
+                }
             }
         }
     }
