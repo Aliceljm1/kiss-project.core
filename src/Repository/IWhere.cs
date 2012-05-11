828172322f2fbe5e61cfa4e57504e87c9494ef83
zhli <zhli@localhost>
2012-05-11 17:13:06 +0800

    ???IWhere??

--- src/Repository/IWhere.cs
+++ src/Repository/IWhere.cs
@@ -1,9 +1,12 @@
-﻿
+﻿using System.Data;
+
 namespace Kiss
 {
     public interface IWhere
     {
         int Count();
+        T Select<T>(string field);
+        DataTable Select(params string[] fields);
         void Delete();
         void Update();
         IWhere Set(string column, string value);
