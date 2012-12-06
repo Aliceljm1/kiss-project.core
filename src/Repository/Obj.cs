403b7cfa367717212b97d33ab11117e16a0c46c6
zhli <zhli@localhost>
2012-12-06 16:44:04 +0800

    fix

--- src/Repository/Obj.cs
+++ src/Repository/Obj.cs
@@ -112,6 +112,7 @@ namespace Kiss
         public int Depth { get; set; }
         public string Name { get; set; }
         public string Title { get; set; }
+        public string Category { get; set; }
         public string Description { get; set; }
         public int SortOrder { get; set; }
         public bool HasChild { get; set; }
