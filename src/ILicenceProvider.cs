a48ca62f271e1e4cd2f801d209f0c8e85bc1e619
zhli <zhli@localhost>
2013-08-26 15:15:53 +0800

    ??licence?????

---
+++ src/ILicenceProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace Kiss
+{
+    public interface ILicenceProvider
+    {
+        bool Check();
+
+        NameValueCollection GetLicenseDetail();
+
+        /// <summary>
+        /// 校验licence失败后的回调函数
+        /// </summary>
+        /// <returns>返回true，继续执行；返回false，中断执行</returns>
+        bool OnLicenceInvalid();
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public sealed class CheckLicenceAttribute : Attribute
+    {
+    }
+}
